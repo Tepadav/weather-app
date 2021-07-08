c3e5451bd93c32cecea877b5c8c008ed1b237f8d
Peter Amir <peteramir5522@gmail.com>
2021-07-08 06:22:38 +0200

    setup weather API and setup a get and post router to store an d fetch data from

--- server.js
+++ server.js
@@ -1,19 +1,43 @@
+
 // Setup empty JS object to act as endpoint for all routes
 projectData = {};
 
 // Require Express to run server and routes
+const express = require('express');
 
 // Start up an instance of app
+const app = express();
 
+// Dependencies
+const bodyParser = require('body-parser');
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
+const cors = require('cors');
+app.use(cors());
 
 // Initialize the main project folder
-app.use(express.static('website'));
+app.use(express.static('./website'));
 
+app.get('/app-data', (req, res) => {
+    res.send(projectData);
+});
 
+app.post('/app-data', (req, res) => {
+    const data = req.body;
+    projectData.temp = data.temperature;
+    projectData.data = data.date;
+    projectData.userResponse = data.userResponse;
+});
+
+const port = 8000
 // Setup Server
+const server = app.listen(port, listening);
+
+function listening() {
+    console.log('server is running');
+    console.log(`running on localhost: ${port}` );
+}
--- website/app.js
+++ website/app.js
@@ -1,5 +1,39 @@
 /* Global Variables */
+const
+    baseUrl = 'https://api.openweathermap.org/data/2.5/weather?zip=',
+    apiKey = '&appid=8a0ff98723c3f28856581b88cdd91bf0',
+    date = document.getElementById('date'),
+    temp = document.getElementById('temp'),
+    content = document.getElementById('content'),
+    generateBtn = document.getElementById('generate');
 
 // Create a new date instance dynamically with JS
-let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+    d = new Date();
+    newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+    
+/* helper functions */
+
+/* Main Functions */
+const generateData = () => {
+    const newZipCode = document.getElementById('zip').value;
+    getWeaterData(baseUrl, newZipCode, apiKey);
+}
+
+const getWeaterData = async (baseUrl, zipCode, apiKey) => {
+    const request = await fetch(baseUrl + zipCode + apiKey);
+    try {
+        const data = await request.json();
+        console.log(data);
+        // return data;
+    } catch(err) {
+        console.log('error', err);
+    }
+}
+/* Event Listeners */
+window.addEventListener('load', () => {
+    console.log(generateBtn);
+    // setup Event Listner to generate data when click on generate button
+    generateBtn.addEventListener('click', () => {
+        generateData();
+    });
+});
